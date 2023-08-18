561638dd27d920b0293e32fe2a44f7c5320f8157
Natesh <123052665+natesh16@users.noreply.github.com>
2023-08-18 17:48:29 +0530

    function

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,27 @@
 import React from 'react'
+
 const Content = () => {
     function handelnamechanger(){
         const name=["Earn","Work","Play With Code"]
         const list =Math.floor(Math.random()*3);
         return name[list]
       }
+const handelclick=()=>{
+  console.log("WELL DONE!")
+}  
+
+const handelclick2=(name)=>{
+  console.log(`WELL DONE! ${name}`)
+}  
+
   return (
-    <div className='h-5/6 flex bg-red-600 w-1/2 mx-auto'>  
-    <p className='w-full my-auto text-center'>Let's{handelnamechanger()}</p>
+    <div className='w-1/2 bg-blue-600 h-5/6 mx-auto'>
+      <div className='flex flex-col'>
+        <p className='my-auto mx-auto text-black text-6xl  font-bold text-center'>Let's {handelnamechanger()}</p>
+        <button onClick={()=>handelclick2("Natesh")}> click Here</button>
+      </div>
     </div>
   )
 }
+
 export default Content
