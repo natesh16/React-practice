faa35489f11a83de45f6efcb024821b8d016e81e
Natesh <123052665+natesh16@users.noreply.github.com>
2023-08-25 17:47:18 +0530

    commit

--- src/Content.js
+++ src/Content.js
@@ -1,30 +1,20 @@
 import React, { useState } from 'react'
 const Content = () => {
-    function handelnamechanger(){
-        const name=["Earn","Work","Play With Code"]
-        const list =Math.floor(Math.random()*3);
-        return name[list]
-      }
-      const [count,setcount]=useState(0)
-
-        function handleincrement(){
-          setcount(count +1)
-        }
-        function handledecremen(){
-          setcount(count -1)
-        }
-
+    const [count,setcount]=useState(0)
+    function handelincrement(){
+        setcount(count+1)
+    }
+    function handeldecrement(){
+        setcount(count-1)
+    }
   return (
-    <div className='w-1/2 bg-blue-600 h-5/6 mx-auto'>
-      <div className='flex h-full'>
-        <div className=' mx-auto my-auto'>
-            <button className='rounded-lg bg-black px-5 py-2' onClick={handledecremen}>-</button>
-            <span className='p-3 text-2xl font-blod bg-red-400 m-5'>{count}</span>
-            <button className='rounded-lg bg-black px-5 py-2' onClick={ handleincrement}>+</button>
-        </div>
-      </div>
-    </div>
+    <main className="bg-black w-screen h-5/6 flex">
+    <div className='flex w-1/2  bg-gray-900 h-full justify-center mx-auto'>
+        <button onClick={handelincrement} className="bg-green-700 my-auto h-10 w-10 rounded-2xl ">+</button>
+            <div className='text-4xl w-20 text-center my-auto'>{count}</div>
+        <button onClick={handeldecrement} className="bg-green-700 my-auto h-10 w-10 rounded-2xl ">-</button>
+    </div> 
+    </main>
   )
 }
-
 export default Content
