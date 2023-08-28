b3d3557547efdce63d1ad887340649b02391dfae
Natesh <natesh0917@gmail.com>
2023-08-28 21:27:54 +0530

    commit

--- src/Content.js
+++ src/Content.js
@@ -20,16 +20,20 @@ const Content = () => {
                 item.id===id ? {...item,checked:!item.checked}:item
             )
             setitems(listitems)
+            localStorage.setItem("Todo-List",JSON.stringify(listitems)) 
         }
+
         const handeldelect=(id)=>{
             const delectitems=items.filter((item)=>
                 item.id!==id)
                 setitems(delectitems)
+                localStorage.setItem("Todo-List",JSON.stringify(delectitems))
             
         }
     
   return (
-      <main className="text-white h-5/6 ">
+      <main className="text-white h-5/6 flex">
+    {(items.length)?(
         <ul className='flex flex-col w-1/2 mx-auto h-full'>
             {items.map((item)=>(
                 <li className='text-center my-auto justify-center mx-28 flex h-20 text-white'>
@@ -37,7 +41,11 @@ const Content = () => {
                         onChange={()=>handlecheck(item.id)}
                         checked={item.checked}
                         />
-                     <label className='text-2xl my-auto mx-auto  '>{item.item}</label>
+                     <label 
+                        style={(item.checked)?
+                        {textDecoration:"line-through"}:null}
+                    className='
+                     text-2xl my-auto mx-auto  '>{item.item}</label>
                      <div className=' my-auto rounded-full text-4xl p-3 bg-green-400 hover:bg-red-600 '><FaTrashRestore
                         onClick={()=>handeldelect(item.id)}
                       className=' border-none outline-none'
@@ -47,6 +55,11 @@ const Content = () => {
                 </li>
             ))}
         </ul>
+        ):
+        (
+            <p className='mx-auto my-auto text-4xl font-bold'>YOUR LIST IS EMPTY</p>
+        )
+    }
     </main>
   )
 }
