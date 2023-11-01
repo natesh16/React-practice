799c02b200e22935a27d200a3bc77d42ba029211
Natesh <natesh0917@gmail.com>
2023-11-01 11:46:06 +0530

    SET UP

---
+++ web_app/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+
+    <div className='flex  w-full h-full'>
+        <h1 className=' mx-auto my-auto text-6xl text-teal-500 font-serif'>
+            Hello World!   
+        </h1>
+    </div>
+  )
+}
+
+export default Header
