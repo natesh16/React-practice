1bd4a7e445965b581a0cfa8f8960e71275a2912d
Natesh <natesh0917@gmail.com>
2023-11-03 19:51:29 +0530

    BROWSER ROUTWER SETUP

--- web_app/src/Postpage.jsx
+++ web_app/src/Postpage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 const Postpage = () => {
   return (
     <main>
-        <h1>Postpage</h1>
+        <h1 className='text-3xl '>Postpage</h1>
+        <ul>
+          <li><Link to="/postpage/1">Post 1</Link></li>
+          <li><Link to="/postpage/2">Post 2</Link></li>
+          <li><Link to="/postpage/3">Post 3</Link></li>
+        </ul>
     </main>
   )
 }
